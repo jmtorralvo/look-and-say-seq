2ed604fd1e45bc5ff3d9cecedde88c4a82ec8b1f
Jose Manuel Torralvo <jm.torralvo@guidesmiths.com>
2019-07-19 16:41:01 +0200

    feat(): Clean code

--- src/index.js
+++ src/index.js
@@ -2,25 +2,13 @@ module.exports = () => {
 
   getSequence = (n) => {
     let count = 0;
-    let arr = (n + '').split('');
-    let pastNumber = arr[0];
+    let numbersArr = (n + '').split('');
+    let lastNum = numbersArr[0];
 
-    let output = (n + '').split('').reduce((acc, curr, i) => {
-      if(curr === pastNumber) {
-        count ++;
-      } 
-      if (curr !== pastNumber) {
-        acc += `${count}${pastNumber}`;
-        pastNumber = curr;
-        count = 1;
-      }
-      if( i === (arr.length - 1)) {
-        acc += `${count}${curr}`;
-      }
-      return acc;
+    return numbersArr.reduce((acc, curr, i) => {
+      (curr === lastNum) ? (count ++) : (acc += `${count}${lastNum}`, lastNum = curr, count = 1);
+      return ( i === (numbersArr.length - 1)) ? `${acc}${count}${curr}` : acc;
     }, '');
-    
-    return output;
 }
 
   return {
